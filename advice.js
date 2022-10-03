b33828ede0ef252d9e91aae22ee98c65727cf2d2
Jane lim <janelim2001@gmail.com>
2022-10-03 20:51:16 +0800

    Advice API Changes

--- advice.js
+++ advice.js
@@ -1,13 +1,13 @@
 function getAdvice()
 {
     console.log("===[START] getAdvice() ===")
-    let advice = document.getElementById("advice").innerHTML;
     api_url_endpoint = "https://api.adviceslip.com/advice"
 
     
     axios.get(api_url_endpoint).then(response =>
         {
-          console.log(response.data);
+          advice = response.data.slip.advice;
+          document.getElementById("advice").innerHTML = advice;
         })
 
     .catch(error =>
